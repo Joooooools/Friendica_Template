ce84654ef53165db2fc27364abe2ae7356cd157d
Missocial <171984337+Joooooools@users.noreply.github.com>
2026-01-29 14:53:04 +0100

    Friendica.de - Theme Onyx Slate
    For Friendica 2026.01

---
+++ friendica.de/friendica_onyx_slate/onyx_slate.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright (C) 2010-2026, the Friendica project
+ * SPDX-FileCopyrightText: 2010-2026 the Friendica project
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Name: Onyx Slate
+ * Licence: AGPL
+ * Author: Jools <jools@friendica.de>
+ * Overwrites: nav_bg, nav_icon_color, background_color, background_image, contentbg_transp
+ * Accented: No
+ * Version: 1.0
+ * Date: 29.01.2026
+ *
+ * CREDITS & ACKNOWLEDGEMENTS:
+ * This theme is a derivative work based on the creativity and technical foundation of the following authors:
+ *
+ * Bookface Theme: Kristi H. @kmh@friendica.world & feb @feb@loma.ml
+ * Frio Black Theme: Hypolite Petovan <hypolite@friendica.mrpetovan.com>
+ */
+
+$nav_bg = '#1C1E26';
+$background_color = '#0e1018';
+$link_color = '#ffffff';
+$nav_icon_color = '#cbd5e1';
+$font_color = '#f1f5f9';
+$font_color_darker = '#94a3b8';
+$font_color_lighter = '#475569';
+$menu_background_hover_color = '#2d323d';
+$background_image = '';
+$contentbg_transp = '0';
