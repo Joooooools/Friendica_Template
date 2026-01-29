4e66354a231c7083018ada39c21d50ab495fb131
Missocial <171984337+Joooooools@users.noreply.github.com>
2026-01-29 20:39:37 +0100

    Missocial.de Friendica Theme Deep Slate

---
+++ missocial.de/missocial_deep_slate/minified/deep_slate.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Copyright (C) 2010-2026, the Friendica project
+ * SPDX-FileCopyrightText: 2010-2026 the Friendica project
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Name: Deep Slate
+ * Licence: AGPL
+ * Author: Jools <jools@friendica.de>
+ * Overwrites: nav_bg, nav_icon_color, background_color, background_image, contentbg_transp
+ * Accented: No
+ * Version: 1.0
+ * Date: 23.01.2026
+ *
+ * CREDITS & ACKNOWLEDGEMENTS:
+ * This theme is a derivative work based on the creativity and technical foundation of the following authors:
+ *
+ * Bookface Dark: Kristi H. @kmh@friendica.world & feb @feb@loma.ml
+ * Frio Black Theme: Hypolite Petovan <hypolite@friendica.mrpetovan.com>
+ */
+
+$nav_bg = '#141928';
+$background_color = '#0f172a';
+$link_color = '#60a5fa';
+$nav_icon_color = '#94a3b8';
+$font_color = '#e2e8f0';
+$font_color_darker = '#94a3b8';
+$font_color_lighter = '#475569';
+$menu_background_hover_color = '#1e2938';
+$background_image = '';
+$contentbg_transp = '0';
