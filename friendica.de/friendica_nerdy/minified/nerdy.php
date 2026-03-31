aa7d29a34f91864bb5c6d471c877c3e235bf9cd6
Jools <171984337+Joooooools@users.noreply.github.com>
2026-03-31 19:40:25 +0200

    Add files via upload

---
+++ friendica.de/friendica_nerdy/minified/nerdy.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Copyright (C) 2010-2026, the Friendica project
+ * SPDX-FileCopyrightText: 2010-2026 the Friendica project
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * Name: Nerdy
+ * Licence: AGPL
+ * Author: Jools <jools@friendica.de>
+ * Overwrites: nav_bg, nav_icon_color, background_color, background_image, contentbg_transp
+ * Accented: No
+ * Version: 1.0
+ * Date: 30.03.2026
+ *
+ * CREDITS & ACKNOWLEDGEMENTS:
+ * This theme is a derivative work based on the creativity and technical foundation of the following authors:
+ *
+ * Bookface Theme: Kristi H. @kmh@friendica.world & feb @feb@loma.ml
+ * Frio Black Theme: Hypolite Petovan <hypolite@friendica.mrpetovan.com>
+ * Onyx Slate Theme: Jools <jools@friendica.de>
+ */
+
+$nav_bg = '#161b22';
+$background_color = '#0d1117';
+$link_color = '#3fb950';
+$nav_icon_color = '#f0f6fc';
+$font_color = '#c9d1d9';
+$font_color_darker = '#8b949e';
+$font_color_lighter = '#30363d';
+$menu_background_hover_color = '#21262d';
+$background_image = '';
+$contentbg_transp = '0';
